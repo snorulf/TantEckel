6a3632a42aa6728d24aa452ec3b57d5612982aba
Jesper Lundbäck <jesperlundback@gmail.com>
2024-10-14 17:49:10 +0200

    Fix build compilation error

--- Assets/TantEckel/Scripts/DrawGizmoIcon.cs
+++ Assets/TantEckel/Scripts/DrawGizmoIcon.cs
@@ -7,10 +7,12 @@ public class DrawGizmoIcon : MonoBehaviour
 {
     [SerializeField] string iconName;
 
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         var sceneView = SceneView.currentDrawingSceneView;
         if (sceneView != null && sceneView.drawGizmos)
             Gizmos.DrawIcon(transform.position, iconName, allowScaling: false);
     }
+#endif
 }
